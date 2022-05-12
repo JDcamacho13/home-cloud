7a7fc2f662678bdd0852c5301ad013830268b65d
José Camacho <j.d.camacho27@gmail.com>
2022-05-12 15:29:40 -0400

    hotfix

--- utils/processPath.js
+++ utils/processPath.js
@@ -1,9 +1,10 @@
+import getConfig from 'next/config'
 const path = require('path')
 
 const slash = process.platform === 'win32' ? '\\' : '/'
 
 export default function processPath (urlPath) {
-  const storage = process.env.NODE_ENV === 'development' ? 'public/store' : '/'
+  const storage = process.env.NODE_ENV === 'development' ? 'public/store' : path.join(getConfig().serverRuntimeConfig.PROJECT_ROOT)
   const relativePath = urlPath || slash
 
   console.log(storage, relativePath)
