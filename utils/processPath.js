8629f934fe35728ec075c716661d26993b28d928
José Camacho <j.d.camacho27@gmail.com>
2022-05-12 14:50:20 -0400

    hotfix

--- utils/processPath.js
+++ utils/processPath.js
@@ -3,7 +3,7 @@ const path = require('path')
 const slash = process.platform === 'win32' ? '\\' : '/'
 
 export default function processPath (urlPath) {
-  const storage = 'store'
+  const storage = ''
   const relativePath = urlPath || slash
 
   const absolutePath = path.join(storage, relativePath)
